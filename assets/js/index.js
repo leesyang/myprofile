69df93c0ed2d48db5a529422b9bedd1960c36a85
Lee Yang <leesyang@gmail.com>
2018-10-19 13:06:01 -0700

    css format, js fade in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,27 @@
-const onClickMore = () => {
-    $('#more').on('click', function() {
-        $('.bio-container').removeClass('hidden');
-    })
-}
+$(document).ready(function() {
+    $('.fadeInLoad').animate({'opacity': '1'},1500)
+})
 
-onClickMore();
+$(document).ready(function() {
+    console.log('running')
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function(){
+    
+        /* Check the location of each desired element */
+        $('.fadeIn').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
